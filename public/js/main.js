361ad3997840a58016110c766626e142f3d714a3
Taner <fricentaurus@gmail.com>
2020-10-22 11:26:06 +0300

    Started sprites and levels ep. 2 part

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ import SpriteSheet from "./SpriteSheet.js";
 import { loadImage, loadLevel } from "./loaders.js";
 
 function drawBackground(background, context, sprites) {
-    console.log('background.ranges', background.ranges)
+    // console.log('background.ranges', background.ranges)
     background.ranges.forEach(([x1, x2, y1, y2]) => {
         for (let x = x1; x < x2; ++x) {
             for (let y = y1; y < y2; ++y) {
@@ -12,27 +12,37 @@ function drawBackground(background, context, sprites) {
     });
 }
 
+function loadBackgroundSprites() {
+    return loadImage('/img/tiles.png')
+        .then(image => {
+            console.log('Image loaded', image)
+            const sprites = new SpriteSheet(image, 16, 16)
+            sprites.define('ground', 0, 0)
+            sprites.define('sky', 3, 23)
+            return sprites
+        })
+}
+
+
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
 
 
 // console.log(context)
 
-loadImage('/img/tiles.png')
-    .then(image => {
-        const sprites = new SpriteSheet(image, 16, 16)
-        sprites.define('ground', 0, 0)
-        sprites.define('sky', 3, 23)
+Promise.all([
 
-        loadLevel('1-1')
-            .then(level => {
-                // console.log(level)
-                level.backgrounds.forEach(background => {
-                    drawBackground(level.backgrounds[0], context, sprites) // 0 is for sky
-                    drawBackground(level.backgrounds[1], context, sprites) // 0 is for sky
-                })
-            })
+])
+    .then
 
+loadLevel('1-1')
+    .then(level => {
+        console.log('Level loaded', level)
+        level.backgrounds.forEach(background => {
+            drawBackground(level.backgrounds[0], context, sprites) // 0 is for sky
+            drawBackground(level.backgrounds[1], context, sprites) // 0 is for sky
+        })
+    })
 
         // draw image on the convas when it is loaded
         // context.drawImage(image,
